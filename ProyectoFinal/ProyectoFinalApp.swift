2e134bb3cc0ff48a46e7c8836830ad0ed14bb088
Kal1219 <kaleb.cortes1208@gmail.com>
2023-08-25 12:17:52 -0600

    Initial Commit

---
+++ ProyectoFinal/ProyectoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoFinalApp.swift
+//  ProyectoFinal
+//
+//  Created by Kaleb on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
